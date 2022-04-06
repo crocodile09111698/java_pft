1a923723dfe36af39680bf25ab3db6e75440a232
Мария Волкова <79218488+crocodile09111698@users.noreply.github.com>
2022-04-06 17:24:37 +0300

    Создана функция, которая вычисляет расстояние мужду двумя точками  при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -2,13 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class Distance {
     public static void main(String[] args) {
-       Point A= new Point(3,5);
-       Point B= new Point(6,5);
-        System.out.println("Расстояние между точками A и B = " + distance(A,B));
+       Point A= new Point(1,2);
+       Point B= new Point(1,2);
+
+        System.out.println("Расстояние между точками A и B = " + Point.distance(A,B));
 
     }
 
-    public  static double distance(Point A,Point B) {
-        return Math.sqrt((B.X - A.X) * (B.X - A.X) + (B.Y - A.Y) * (B.Y - A.Y) );
-    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,11 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
     public double X;
-   public double Y;
-    public Point (double X, double Y) {
+    public double Y;
+
+    public Point(double X, double Y) {
         this.X = X;
         this.Y = Y;
     }
 
+    public static double distance(Point A, Point B) {
+        return Math.sqrt((B.X - A.X) * (B.X - A.X) + (B.Y - A.Y) * (B.Y - A.Y));
+
+    }
 }
+
 
